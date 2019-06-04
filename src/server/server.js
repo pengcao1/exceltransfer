c0936e0fbd45301267a79d02abcec8d501e53d02
Peng.Cao <pcao@thoughtworks.com>
2019-06-04 21:06:19 +0800

    add interface for backend to deal excel file

--- src/server/server.js
+++ src/server/server.js
@@ -9,7 +9,9 @@ var storage = multer.diskStorage({
         cb(null, 'public')
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
+        const fileName = Date.now() + '-' + file.originalname;
+        cb(null, fileName)
+        convertExcel(fileName);
     }
 })
 
@@ -19,23 +21,67 @@ app.get('/', function (req, res) {
     return res.send('Hello Server')
 })
 app.post('/upload', function (req, res) {
-
     upload(req, res, function (err) {
-
         if (err instanceof multer.MulterError) {
+            console.log(err)
             return res.status(500).json(err)
             // A Multer error occurred when uploading.
         } else if (err) {
+            console.log(err ,"--")
             return res.status(500).json(err)
             // An unknown error occurred when uploading.
         }
 
-        console.log(req.file);
+
+        console.log(req.file, "99");
         return res.status(200).send(req.file)
         // Everything went fine.
     })
 });
 
-app.listen(8000, function () {
-    console.log('App running on port 8000');
+
+const convertExcel = (fileName) => {
+    console.log("11 in convertExcel fileName=%s", fileName);
+    const excelToJson = require('convert-excel-to-json');
+    const fs = require('fs');
+    const result = excelToJson({
+        sourceFile: 'public/'+fileName,
+        sheets: [{
+            name: 'Purchase map',
+            header: {
+                rows: 1
+            },
+            columnToKey: {
+                A: 'Base Course Type',
+                B: 'ISBN',
+                C: 'Course name',
+                D: 'Upsell in-app purchase ISBN',
+                E: 'Upsell course name',
+                F: 'Upsell2 in-app purchase ISBN',
+                G: 'Upsell2 course name',
+                H: 'Upgrade in-app purchase ISBN',
+                I: 'Upgrade course name'
+            }
+        }, {
+            name: 'Format map',
+            header: {
+                rows: 1
+            },
+            columnToKey: {
+                A: 'Base Course Type',
+                B: 'ISBN',
+                C: 'Course name - Upsell Card & Library',
+                D: 'Course name - Learn Page',
+                E: 'Course Description - Upsell Card',
+                F: 'Other format 1 (Upsell) ISBN',
+                G: 'Other format 2 (Upgrade) ISBN',
+                H: 'Other format 3 (DVD) ISBN'
+            }
+        }]
+    });
+
+
+}
+app.listen(8100, function () {
+    console.log('App running on port 8100');
 });
